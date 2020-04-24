3f2e967412ea704ca957b2e73cac8377c675214e
miguel alejandro martinez olivarez <miguelesalador@gmail.com>
2020-04-23 22:15:20 -0400

    scss

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Header from './components/layout/header/Header';
+
+function App() {
+  return (
+    <div className="h-12-m col-12-m">
+
+    <Header></Header>
+    </div>
+     
+  );
+}
+
+export default App;
---
+++ src/components/layout/header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header className="h-1-m col-12-m">
+            Aplication Header
+        </header>
+    )
+}
+
+export default Header
